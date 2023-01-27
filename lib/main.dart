d8024f128f74c2f5086956aef0c0bde111bdd386
Wasiullah <waseeullah14@gmail.com>
2023-01-28 01:00:31 +0500

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Covid Tracker',
       theme: ThemeData(
         brightness: Brightness.dark,
